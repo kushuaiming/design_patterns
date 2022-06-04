690b307e0091ad08e6163f2e6472d235f80c7201
ming.xu <ming.xu@allride.ai>
2022-06-04 15:52:44 +0800

    refactor: chain of responsibility

--- behavioral_patterns/chain_of_responsibility.cc
+++ behavioral_patterns/chain_of_responsibility.cc
@@ -7,6 +7,7 @@
  */
 
 #include <iostream>
+#include <memory>
 #include <string>
 #include <vector>
 /**
@@ -14,68 +15,53 @@
  * It also declares a method for executing a request.
  */
 class Handler {
- public:
-  virtual Handler *SetNext(Handler *handler) = 0;
-  virtual std::string Handle(std::string request) = 0;
-};
-/**
- * The default chaining behavior can be implemented inside a base handler class.
- */
-class AbstractHandler : public Handler {
-  /**
-   * @var Handler
-   */
  private:
-  Handler *next_handler_;
+  std::shared_ptr<Handler> successor_;
 
  public:
-  AbstractHandler() : next_handler_(nullptr) {}
-  Handler *SetNext(Handler *handler) override {
-    next_handler_ = handler;
-    // Returning a handler from here will let us link handlers in a convenient
-    // way like this:
-    // $monkey->setNext($squirrel)->setNext($dog);
+  virtual std::shared_ptr<Handler> SetSuccessor(
+      std::shared_ptr<Handler> handler) {
+    successor_ = handler;
     return handler;
   }
-  std::string Handle(std::string request) override {
-    if (next_handler_) {
-      return next_handler_->Handle(request);
-    }
-
-    return {};
+  virtual std::string HandleRequest(std::string request) {
+    if (successor_) {
+      return successor_->HandleRequest(request);
+    } else return "";
   }
 };
+
 /**
  * All Concrete Handlers either handle a request or pass it to the next handler
  * in the chain.
  */
-class MonkeyHandler : public AbstractHandler {
+class MonkeyHandler : public Handler {
  public:
-  std::string Handle(std::string request) override {
+  std::string HandleRequest(std::string request) override {
     if (request == "Banana") {
       return "Monkey: I'll eat the " + request + ".\n";
     } else {
-      return AbstractHandler::Handle(request);
+      return Handler::HandleRequest(request);
     }
   }
 };
-class SquirrelHandler : public AbstractHandler {
+class SquirrelHandler : public Handler {
  public:
-  std::string Handle(std::string request) override {
+  std::string HandleRequest(std::string request) override {
     if (request == "Nut") {
       return "Squirrel: I'll eat the " + request + ".\n";
     } else {
-      return AbstractHandler::Handle(request);
+      return Handler::HandleRequest(request);
     }
   }
 };
-class DogHandler : public AbstractHandler {
+class DogHandler : public Handler {
  public:
-  std::string Handle(std::string request) override {
+  std::string HandleRequest(std::string request) override {
     if (request == "MeatBall") {
       return "Dog: I'll eat the " + request + ".\n";
     } else {
-      return AbstractHandler::Handle(request);
+      return Handler::HandleRequest(request);
     }
   }
 };
@@ -83,11 +69,11 @@ class DogHandler : public AbstractHandler {
  * The client code is usually suited to work with a single handler. In most
  * cases, it is not even aware that the handler is part of a chain.
  */
-void ClientCode(Handler &handler) {
+void ClientCode(std::shared_ptr<Handler> handler) {
   std::vector<std::string> food = {"Nut", "Banana", "Cup of coffee"};
   for (const std::string &f : food) {
     std::cout << "Client: Who wants a " << f << "?\n";
-    const std::string result = handler.Handle(f);
+    const std::string result = handler->HandleRequest(f);
     if (!result.empty()) {
       std::cout << "  " << result;
     } else {
@@ -99,24 +85,19 @@ void ClientCode(Handler &handler) {
  * The other part of the client code constructs the actual chain.
  */
 int main() {
-  MonkeyHandler *monkey = new MonkeyHandler;
-  SquirrelHandler *squirrel = new SquirrelHandler;
-  DogHandler *dog = new DogHandler;
-  monkey->SetNext(squirrel)->SetNext(dog);
+  std::shared_ptr<Handler> monkey(new MonkeyHandler);
+  std::shared_ptr<Handler> squirrel(new SquirrelHandler);
+  std::shared_ptr<Handler> dog(new DogHandler);
+  monkey->SetSuccessor(squirrel)->SetSuccessor(dog);
 
   /**
    * The client should be able to send a request to any handler, not just the
    * first one in the chain.
    */
   std::cout << "Chain: Monkey > Squirrel > Dog\n\n";
-  ClientCode(*monkey);
+  ClientCode(monkey);
   std::cout << "\n";
   std::cout << "Subchain: Squirrel > Dog\n\n";
-  ClientCode(*squirrel);
-
-  delete monkey;
-  delete squirrel;
-  delete dog;
-
+  ClientCode(squirrel);
   return 0;
 }
