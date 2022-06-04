f8ff2862aeff52337efdf80733827546fd45880b
ming.xu <ming.xu@allride.ai>
2022-06-04 15:33:29 +0800

    docs: add intent for chain of responsibility

--- behavioral_patterns/chain_of_responsibility.cc
+++ behavioral_patterns/chain_of_responsibility.cc
@@ -1,3 +1,11 @@
+/**
+ * Chain of Responsibility Design Pattern
+ *
+ * Avoid coupling the sender of a request to its receiver by giving more than
+ * one object a chance to handle the request. Chain the receiving objects and
+ * pass the request along the chain until an object handles it.
+ */
+
 #include <iostream>
 #include <string>
 #include <vector>
