48fc36eed124261cd4bd5693e0a149973937b1ac
ming.xu <ming.xu@allride.ai>
2022-06-04 11:07:47 +0800

    refactor: modify chain of responsibility

--- behavioral_patterns/chain_of_responsibility.cc
+++ behavioral_patterns/chain_of_responsibility.cc
@@ -23,15 +23,15 @@ class AbstractHandler : public Handler {
  public:
   AbstractHandler() : next_handler_(nullptr) {}
   Handler *SetNext(Handler *handler) override {
-    this->next_handler_ = handler;
+    next_handler_ = handler;
     // Returning a handler from here will let us link handlers in a convenient
     // way like this:
     // $monkey->setNext($squirrel)->setNext($dog);
     return handler;
   }
   std::string Handle(std::string request) override {
-    if (this->next_handler_) {
-      return this->next_handler_->Handle(request);
+    if (next_handler_) {
+      return next_handler_->Handle(request);
     }
 
     return {};
