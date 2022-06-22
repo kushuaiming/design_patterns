f6db3c5bdf47dd10c45ba2ea3b755d32192393df
ming.xu <ming.xu@allride.ai>
2022-06-22 20:38:23 +0800

    docs: template

--- behavioral_patterns/template.cc
+++ behavioral_patterns/template.cc
@@ -1,15 +1,9 @@
+// Intent: Define the skeleton of an algorithm in an operation, deferring some
+// steps to subclass. Template Method lets subclasses redefine certain steps of
+// an algorithm without changing the algorithm's structure.
+
 #include <iostream>
-/**
- * The Abstract Class defines a template method that contains a skeleton of some
- * algorithm, composed of calls to (usually) abstract primitive operations.
- *
- * Concrete subclasses should implement these operations, but leave the template
- * method itself intact.
- */
 class AbstractClass {
-  /**
-   * The template method defines the skeleton of an algorithm.
-   */
  public:
   void TemplateMethod() const {
     BaseOperation1();
@@ -20,9 +14,6 @@ class AbstractClass {
     BaseOperation3();
     Hook2();
   }
-  /**
-   * These operations already have implementations.
-   */
  protected:
   void BaseOperation1() const {
     std::cout << "AbstractClass says: I am doing the bulk of the work\n";
@@ -35,24 +26,12 @@ class AbstractClass {
     std::cout
         << "AbstractClass says: But I am doing the bulk of the work anyway\n";
   }
-  /**
-   * These operations have to be implemented in subclasses.
-   */
   virtual void RequiredOperations1() const = 0;
   virtual void RequiredOperation2() const = 0;
-  /**
-   * These are "hooks." Subclasses may override them, but it's not mandatory
-   * since the hooks already have default (but empty) implementation. Hooks
-   * provide additional extension points in some crucial places of the
-   * algorithm.
-   */
   virtual void Hook1() const {}
   virtual void Hook2() const {}
 };
-/**
- * Concrete classes have to implement all abstract operations of the base class.
- * They can also override some operations with a default implementation.
- */
+
 class ConcreteClass1 : public AbstractClass {
  protected:
   void RequiredOperations1() const override {
@@ -62,9 +41,7 @@ class ConcreteClass1 : public AbstractClass {
     std::cout << "ConcreteClass1 says: Implemented Operation2\n";
   }
 };
-/**
- * Usually, concrete classes override only a fraction of base class' operations.
- */
+
 class ConcreteClass2 : public AbstractClass {
  protected:
   void RequiredOperations1() const override {
@@ -77,11 +54,7 @@ class ConcreteClass2 : public AbstractClass {
     std::cout << "ConcreteClass2 says: Overridden Hook1\n";
   }
 };
-/**
- * The client code calls the template method to execute the algorithm. Client
- * code does not have to know the concrete class of an object it works with, as
- * long as it works with objects through the interface of their base class.
- */
+
 void ClientCode(AbstractClass *class_) {
   // ...
   class_->TemplateMethod();
